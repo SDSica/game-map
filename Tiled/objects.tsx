acf19fb1cea6959a751c7967139e88cb925f13ac
SDSica <73423818+SDSica@users.noreply.github.com>
2026-03-11 15:02:57 +0100

    Delete Tiled directory

--- Tiled/objects.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="objects" tilewidth="12" tileheight="12" tilecount="1144" columns="44">
- <image source="objects.png" width="528" height="320"/>
-</tileset>
